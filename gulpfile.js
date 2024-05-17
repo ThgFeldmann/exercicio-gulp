929dd3357fab55bb0d85626a242baec22b722b0f
Thiago Feldmann <thiagofeldmann06@gmail.com>
2024-05-17 13:52:04 -0300

    fix-error
    erro ao executar "npm run gulp". Não havia opção de tarefa default. Agora ao executar o comando mencionado, é executado todas as tarefas. Ainda possui a opção de executar tarefas individuais com "npm run gulp + [nome tarefa]".

--- gulpfile.js
+++ gulpfile.js
@@ -30,3 +30,9 @@ exports.images = comprimeImagens;
 exports.watch = function() {
     gulp.watch('./source/styles/*.scss', gulp.series(compilaSass));
 }
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
+}
